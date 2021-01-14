96546b8d8f13627a4633f5b3bf365dcdf7755934
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2021-01-14 12:14:44 +0100

    Introduced stratified holdouts generator method

--- crr_prediction/utils/__init__.py
+++ crr_prediction/utils/__init__.py
@@ -1,6 +1,8 @@
 """Generic utilities used in the package."""
 from .normalize_epigenomic_data import normalize_epigenomic_data
+from .stratified_holdouts import stratified_holdouts
 
 __all__ = [
-    "normalize_epigenomic_data"
+    "normalize_epigenomic_data",
+    "stratified_holdouts"
 ]
