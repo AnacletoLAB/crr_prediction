231c7826b67644311d2da1e9875e66e632d99ca4
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2021-01-16 22:56:02 +0100

    Reduced hyper-parameters space

--- crr_prediction/meta_models/cnn.py
+++ crr_prediction/meta_models/cnn.py
@@ -15,19 +15,20 @@ def build_cnn_meta_model(window_size: int) -> CNN1DMetaModel:
     CNN1D model.
     """
     return CNN1DMetaModel(
-        blocks=3,
+        blocks=2,
         input_shape=(window_size, 4),
         meta_layer_kwargs=dict(
             batch_normalization=False,
+            dropout=True,
             residual=True,
-            max_kernel_size=10,
+            max_kernel_size=8,
             min_kernel_size=2,
-            max_filters=256
+            max_filters=128
         ),
         top_ffnn_meta_model_kwargs=dict(
-            blocks=3,
+            blocks=2,
             meta_layer_kwargs=dict(
-                max_units=256,
+                max_units=128,
                 batch_normalization=True,
                 residual=True,
                 dropout=True,
