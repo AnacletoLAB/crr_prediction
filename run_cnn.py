ebd6867a7c81ebe2e0f43accbda15bc322abf88a
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2021-01-17 21:25:41 +0100

    Updated models

--- run_cnn.py
+++ run_cnn.py
@@ -1,9 +1,11 @@
 """Training of cnn models."""
+import ray
 from crr_prediction import train_meta_models
 from crr_prediction.meta_models import build_cnn_meta_model
 from crr_prediction.sequences import build_cnn_sequences
 
 if __name__ == "__main__":
+    ray.init()
     train_meta_models(
         build_cnn_sequences,
         build_cnn_meta_model
