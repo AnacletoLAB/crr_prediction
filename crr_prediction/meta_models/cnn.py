235adff8b5b058737d20100a42fc33236774d6f1
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2021-01-17 16:15:10 +0100

    Simplified model

--- crr_prediction/meta_models/cnn.py
+++ crr_prediction/meta_models/cnn.py
@@ -15,7 +15,7 @@ def build_cnn_meta_model(window_size: int) -> CNN2DMetaModel:
     CNN1D model.
     """
     return CNN2DMetaModel(
-        blocks=3,
+        blocks=2,
         input_shape=(window_size, 4),
         target_shape=(window_size, 4, 1),
         meta_layer_kwargs=dict(
@@ -24,6 +24,10 @@ def build_cnn_meta_model(window_size: int) -> CNN2DMetaModel:
             residual=True,
             max_filters=128,
             min_x_strides=2,
+            min_x_kernel_size=2,
+            max_x_kernel_size=8,
+            min_y_kernel_size=1,
+            max_y_kernel_size=1,
             l1_regularization=1e-4,
             l2_regularization=1e-4,
             activity_regularizer=True,
@@ -31,7 +35,7 @@ def build_cnn_meta_model(window_size: int) -> CNN2DMetaModel:
             bias_regularizer=True,
         ),
         top_ffnn_meta_model_kwargs=dict(
-            blocks=3,
+            blocks=2,
             meta_layer_kwargs=dict(
                 max_units=64,
                 batch_normalization=True,
