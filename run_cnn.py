8f06124fbbda8900ac8f9b32865a145180b3b501
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2021-01-17 21:32:01 +0100

    Updated genome

--- run_cnn.py
+++ run_cnn.py
@@ -4,11 +4,14 @@ from crr_prediction import train_meta_models
 from crr_prediction.meta_models import build_cnn_meta_model
 from crr_prediction.sequences import build_cnn_sequences
 from meta_models.utils import patch_global_checkpoints_interval
+from ucsc_genomes_downloader import Genome
 
 if __name__ == "__main__":
     patch_global_checkpoints_interval()
     ray.init()
     train_meta_models(
         build_cnn_sequences,
-        build_cnn_meta_model
+        build_cnn_meta_model,
+        total_threads=16,
+        genome=Genome("hg38")
     ).to_csv("cnn.csv", index=False)
