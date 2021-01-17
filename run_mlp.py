6a824864e8220038c4ab953aff7dd68d0f1ae8fc
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2021-01-17 21:28:10 +0100

    Updated models

--- run_mlp.py
+++ run_mlp.py
@@ -3,8 +3,10 @@ import ray
 from crr_prediction import train_meta_models
 from crr_prediction.meta_models import build_mlp_meta_model, build_cnn_meta_model
 from crr_prediction.sequences import build_cnn_sequences, build_mlp_sequences
+from meta_models.utils import patch_global_checkpoints_interval
 
 if __name__ == "__main__":
+    patch_global_checkpoints_interval()
     ray.init()
     train_meta_models(
         build_mlp_sequences,
